66f75564620b41899524dfea8661ff8b611ce7dd
Yash Sharma <yash95kumar@gmail.com>
2022-06-19 20:41:11 -0700

    Time: 37 ms (70.92%), Space: 14 MB (11.71%) - LeetHub

---
+++ 246-strobogrammatic-number/246-strobogrammatic-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        pairs = set([('0','0'), ('1','1'), ('6','9'), ('8','8'), ('9','6')])
+        
+        i=0
+        j = len(num)-1
+        
+        while i<=j:
+            if (num[i], num[j]) not in pairs:
+                return False
+            i+=1
+            j-=1
+        return True
