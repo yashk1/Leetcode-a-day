8a7c96606e478aebc4a8563e961e24f20382d10e
Yash Sharma <yash95kumar@gmail.com>
2022-06-22 17:44:39 -0700

    Time: 33 ms (95.01%), Space: 14.1 MB (33.38%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,7 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        op = [[]]
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:  
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
         
-        for number in nums:
-            op = op + [cur + [number] for cur in op]
-        return op
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
