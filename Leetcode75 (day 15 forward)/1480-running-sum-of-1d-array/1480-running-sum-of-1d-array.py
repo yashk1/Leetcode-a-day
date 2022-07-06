f07f37e3100efb408e16de6b163dee3616af1e2d
Yash Sharma <Yash95kumar@gmail.com>
2022-07-05 21:11:41 -0700

    day 16

--- Leetcode75 (day 15 forward)/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ Leetcode75 (day 15 forward)/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -3,7 +3,3 @@ class Solution:
         for i in range(1, len(nums)):
             nums[i] += nums[i-1]
         return nums
-
-#complexity of this function is O(n) as the for loop will run for the lenght of nums array
-#Time complexity - O(n)
-#Space complexity  - O(1)
