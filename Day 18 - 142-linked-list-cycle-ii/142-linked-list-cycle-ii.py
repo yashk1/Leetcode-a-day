84d9c889c096a81f1e7d07bcac5a0b143e291abc
Yash Sharma <Yash95kumar@gmail.com>
2022-07-06 10:05:39 -0700

    day 18

---
+++ Day 18 - 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        hashset = set()
+        
+        if head is None:
+            return None
+        
+        while head is not None:
+            if head in hashset:
+                return head
+            hashset.add(head)
+            head = head.next
+            
+            
+        
+        return None
+        
+#time complexity - O(n)
+#space complexity - O(n) since we are creating a hashset
