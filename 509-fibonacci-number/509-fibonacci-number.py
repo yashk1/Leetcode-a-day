1d08e6d0c4015ad8f1ee073321c60b5e3be51307
Yash Sharma <yash95kumar@gmail.com>
2022-07-13 10:22:14 -0700

    Time: 3314 ms (5.01%), Space: 14 MB (9.48%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,9 +1,15 @@
 class Solution:
+    
     def fib(self, n: int) -> int:
+        dict = {} #this is memoized recursive
+
         if n == 0:
             return 0
         if n == 1:
             return 1
         
-        return self.fib(n-1) + self.fib(n-2)
+        if n-1 not in dict: dict[n-1] = self.fib(n-1)
+        if n-2 not in dict: dict[n-2] = self.fib(n-2)
+        
+        return dict[n-1] + dict[n-2]
             
