e500b8e3c3f4b82225b6725b84a34d31a45613c6
Yash Sharma <yash95kumar@gmail.com>
2022-07-20 12:23:47 -0700

    Time: 40 ms (72.90%), Space: 13.9 MB (73.78%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,24 +1,47 @@
-class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        s1=[]
-        s2 = []
-        
-        for i in s:
-            if i == '#':
-                if s1:
-                    s1.pop()
-                    
-            else:
-                s1.append(i)
-                
-        for i in t:
-            if i == '#':
-                if s2:
-                    s2.pop()
-            else:
-                s2.append(i)
-        
-        return s1 == s2
+
     
-#Time: O(M + N), where M <= 200 is length of string s, N <= 200 is length of string t.
-#Space: O(M + N)
+class Solution:
+	def backspaceCompare(self, S, T):
+		i = len(S) - 1			# Traverse from the end of the strings
+		j = len(T) - 1
+
+		skipS = 0              # The number of backspaces required till we arrive at a valid character
+		skipT = 0
+
+		while i >= 0 or j >= 0:
+			while i >= 0:					# Ensure that we are comparing a valid character in S
+				if S[i] == "#" :
+					skipS += 1				# If not a valid character, keep times we must backspace.
+					i = i - 1
+
+				elif skipS > 0:
+					skipS -= 1				# Backspace the number of times calculated in the previous step
+					i = i - 1
+
+				else:
+					break
+
+			while j >= 0:					# Ensure that we are comparing a valid character in T
+					if T[j] == "#":
+						skipT += 1			# If not a valid character, keep times we must backspace.
+						j = j - 1
+
+					elif skipT > 0:
+						skipT -= 1			# Backspace the number of times calculated in the previous step
+						j = j - 1
+
+					else:
+						break
+
+			print("Comparing", S[i], T[j])		# Print out the characters for better understanding.
+
+			if i>= 0 and j >= 0 and S[i] != T[j]:    # Compare both valid characters. If not the same, return False.
+				return False
+
+			if (i>=0) != (j>=0):		# Also ensure that both the character indices are valid. If it is not valid,
+				return False			#  it means that we are comparing a "#" with a valid character.
+
+			i = i - 1
+			j = j - 1
+
+		return True					# This means both the strings are equivalent.
