d13c668ae9d0dee8207f978aaa08165015ec4f54
Yash Sharma <Yash95kumar@gmail.com>
2022-07-25 13:19:58 -0700

    day 30

---
+++ Day 29 - random practice questions/1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        #return number of steps to take from num to 0
+        #even number divide by 2
+        #odd number subtract 1
+        
+        steps = 0
+        while num > 0 :
+            if num % 2 == 0:
+                num = num // 2
+                steps += 1
+            else:
+                num = num - 1
+                steps +=1
+        return steps
+            
+#time - O(logn)
+#space - O(1)
